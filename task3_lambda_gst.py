b57e398fc797312f89ae4edefbe58f0a9425e472
21241A05L6 <photospvn@gmail.com>
2026-03-12 00:23:33 +0530

    Delete task3_lambda_gst.py

--- task3_lambda_gst.py
+++
@@ -1,9 +0,0 @@
-# Task 3: Lambda GST
-
-gst = lambda price: price + (0.18 * price)
-
-print(gst(100))
-
-final_price = lambda price, discount: (price - price * discount/100) * 1.18
-
-print(final_price(1000, 10))
