174a2a8f06be909f7b2b23d53e37a89b2c5fb211
21241A05L6 <photospvn@gmail.com>
2026-03-12 17:20:44 +0530

    Delete numpy_assignment directory

--- numpy_assignment/task3_numpy_formulas.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-values = np.array([2, 4, 6, 8, 10])
-
-print("Square root:", np.sqrt(values))
-print("Exponential:", np.exp(values))
-print("Natural log:", np.log(values))
-print("Sum:", np.sum(values))
-print("Cumulative sum:", np.cumsum(values))
